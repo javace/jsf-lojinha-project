d69e6ddfbb2b9bed22065697018f4b413326238e
Rafael Ponte <rponte@gmail.com>
2010-11-22 23:41:57 -0300

    Remove anotação desnecessária - sujeira

--- src/org/javace/social/lojinha/model/Usuario.java
+++ src/org/javace/social/lojinha/model/Usuario.java
@@ -3,7 +3,6 @@ package org.javace.social.lojinha.model;
 import java.io.Serializable;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
@@ -12,7 +11,6 @@ public class Usuario implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	@Id
-	@GeneratedValue
 	private String login;
 	private String nome;
 	private String senha;
