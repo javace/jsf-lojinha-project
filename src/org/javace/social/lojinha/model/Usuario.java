c4d903fa7bb9d05299625e3169743904bca77ec3
Rafael Ponte <rponte@gmail.com>
2010-11-22 23:09:02 -0300

    Remove anotação desnecessária - sujeira

--- src/org/javace/social/lojinha/model/Usuario.java
+++ src/org/javace/social/lojinha/model/Usuario.java
@@ -12,7 +12,6 @@ public class Usuario implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	@Id
-	@GeneratedValue
 	private String login;
 	private String nome;
 	private String senha;
